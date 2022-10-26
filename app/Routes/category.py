0d82598c73f71c2462e427a3dcdea864624a98ba
xShiro2 <daryllejumalon16@gmail.com>
2022-10-26 10:20:49 +0800

    feat: add DELETE method

--- app/Routes/category.py
+++ app/Routes/category.py
@@ -5,7 +5,7 @@ from flask_login import login_required, current_user
 from app.Components.response import Response
 
 @login_required
-@app.route('/api/v1/shop/category', methods=['POST', 'GET', 'DELETE'])
+@app.route('/api/v1/shop/category', methods=['POST', 'GET'])
 def category():
     if current_user.user_type == 'Buyer':
         return Response(
@@ -16,6 +16,12 @@ def category():
     if request.method == 'POST':
         data = request.get_json()
         shop  = Shop.query.filter_by(seller_id=current_user.id).first()
+
+        if Category.query.filter_by(name = data['categoryName']).first():
+            return Response(
+                status=409,
+                message="error",
+            )
         
         category = Category(
             shop_id = shop.id,
@@ -49,5 +55,16 @@ def category():
         
         return Response(
             data=res, 
+            status=200
+        )
+    
+@login_required
+@app.route('/api/v1/shop/category/<id>', methods=['DELETE'])
+def delete(id):
+    if request.method == 'DELETE':
+        category = Category.query.get_or_404(id)
+        category.delete()
+
+        return Response(
             status=200
         )
