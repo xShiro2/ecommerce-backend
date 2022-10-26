2dbf628f4795f3dfe8352e47f52df22f9bd96abf
xShiro2 <daryllejumalon16@gmail.com>
2022-10-26 13:16:58 +0800

    feat: add admin route

--- app/Routes/user.py
+++ app/Routes/user.py
@@ -12,3 +12,19 @@ def users():
             message= "",
             data = current_user.to_dict(),
         )
+
+@app.route('/api/v1/user/admin', methods=['GET', 'POST', 'DELETE'])
+@login_required
+def admin():
+    if current_user.user_type == 'Buyer':
+        return Response(
+            status=403,
+            message="error",
+        )
+
+    if request.method == 'GET':
+        return Response(
+            status = 200,
+            message= "",
+            data = current_user.to_dict(),
+        )
