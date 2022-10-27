4310d44cc0a743829178f12ab3a4c7a2a7695120
xShiro2 <daryllejumalon16@gmail.com>
2022-10-27 10:55:24 +0800

    feat!: check if user is authenticated

--- app/Routes/user.py
+++ app/Routes/user.py
@@ -2,15 +2,19 @@ from app import app
 from flask_login import login_required, current_user
 from flask import request
 from app.Components.response import Response
-
+    
 @app.route('/api/v1/user', methods=['GET', 'POST', 'DELETE'])
 @login_required
 def users():
     if request.method == 'GET':
+        if not current_user.is_authenticated:
+            return Response(
+                status = 401
+            )
         return Response(
             status = 200,
             message= "",
-            data = current_user.to_dict(),
+            data = current_user.to_dict(exclude=['password']),
         )
 
 @app.route('/api/v1/user/admin', methods=['GET', 'POST', 'DELETE'])
@@ -23,8 +27,13 @@ def admin():
         )
 
     if request.method == 'GET':
+        if not current_user.is_authenticated:
+            return Response(
+                status = 401
+            )
         return Response(
             status = 200,
             message= "",
-            data = current_user.to_dict(),
+            data = current_user.to_dict(exclude=['password']),
         )
+
