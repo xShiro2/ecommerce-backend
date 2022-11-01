b9ab62bf71d39b32ba7e21a93115bf33c3436d4c
xShiro2 <daryllejumalon16@gmail.com>
2022-11-01 13:00:05 +0800

    feat: rescale img to 800px

--- app/Components/image_handler.py
+++ app/Components/image_handler.py
@@ -2,13 +2,17 @@ import os
 from PIL import Image
 
 IMAGE_FOLDER = 'IMAGE_FOLDER'
+WIDTH = 800
 
 def save_img(img):
     if not os.path.exists(IMAGE_FOLDER):
         os.makedirs(IMAGE_FOLDER)
 
     path = IMAGE_FOLDER + "/" + img.filename + '.png'
+
     im = Image.open(img.stream)
+    newHeight = int(WIDTH * im.height/im.width)
+    im = im.resize((WIDTH, newHeight), resample=Image.LANCZOS)
     im.save(path, 'PNG')
 
     return path
