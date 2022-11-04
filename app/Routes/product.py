77b05a0004f191b5a7f3c49a7ac368a17792892a
xShiro2 <daryllejumalon16@gmail.com>
2022-11-04 15:54:55 +0800

    chore: refractor

--- app/Routes/product.py
+++ app/Routes/product.py
@@ -143,52 +143,51 @@ def product(id):
 
 @app.route('/api/v1/products', methods=['GET'])
 def getproducts():
+    minNumber = 12
     if request.method == 'GET':
-        activeProducts = QuantityStatus.query.filter_by(status=True).all()
+        prod_filter = request.args.get('filter')
+        page = int(request.args.get('page'))
 
-        products=[]
-        for id in activeProducts:
-            product = Product.query.filter_by(id=id.product).first()
+        prod_len = Product.query.join(QuantityStatus.query.filter_by(status=True)).all()
+        products = Product.query.join(QuantityStatus.query.filter_by(status=True)).paginate(page=page, per_page=minNumber)
+        if prod_filter:
+            gender = []
+            if prod_filter == 'men':
+                gender = Gender.query.filter_by(name='Male').first()
+            if prod_filter == 'women':
+                gender = Gender.query.filter_by(name='Female').first()
+            if prod_filter == 'kids':
+                gender = Gender.query.filter_by(name='Kids').first()
+            if prod_filter == 'best':
+                pass
+            if prod_filter == 'featured':
+                pass
+            if prod_filter == 'latest':
+                products = Product.query.order_by(Product.dateCreated.desc()).join(QuantityStatus.query.filter_by(status=True)).paginate(page=page, per_page=minNumber)
+                prod_len = Product.query.order_by(Product.dateCreated.desc()).all()
+            if gender:
+                products = Product.query.filter_by(gender=gender.id).join(QuantityStatus.query.filter_by(status=True)).paginate(page=page, per_page=minNumber)
+                prod_len = Product.query.filter_by(gender=gender.id).join(QuantityStatus.query.filter_by(status=True)).all()
+        else:
+            pass
+
+        prods=[]
+        for product in products:
+            quantityStatus = QuantityStatus.query.filter_by(product=product.id).first()
             prod = product.to_dict(exclude='image')
             prod['category'] = product.cat.name
             prod['gender'] = product.gen.name
-            prod['quantity'] = id.quantity
+            prod['quantity'] = quantityStatus.quantity
 
-            products.append(prod)
+            prods.append(prod)
 
         return Response(
-            data=products,
+            data=prods,
+            message=len(prod_len),
             status=200
         )
 
-@app.route('/api/v1/products/<filter>', methods=['GET'])
-def getFilteredProduct(filter):
-    if request.method == 'GET':
-        if filter == 'men':
-            gender = Gender.query.filter_by(name='Male').first()
-        if filter == 'women':
-            gender = Gender.query.filter_by(name='Female').first()
-        if filter == 'kids':
-            gender = Gender.query.filter_by(name='Kids').first()
-
-        filtered = Product.query.filter_by(gender=gender.id).all()
-
-        products=[]
-        for product in filtered:
-            quantityStatus = QuantityStatus.query.filter_by(product=product.id).first()
-            if quantityStatus.status:
-                prod = product.to_dict(exclude='image')
-                prod['category'] = product.cat.name
-                prod['gender'] = product.gen.name
-                prod['quantity'] = quantityStatus.quantity
-
-                products.append(prod)
-
-        return Response(
-            data=products,
-            status=200
-        )
-
+        
 @app.route('/api/v1/product/<id>', methods=['GET'])
 def displayproduct(id):
     if request.method == 'GET':
