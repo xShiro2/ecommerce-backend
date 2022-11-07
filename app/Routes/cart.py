977d3152d009da99565d2176e8bc6a01df620962
xShiro2 <daryllejumalon16@gmail.com>
2022-11-07 13:49:05 +0800

    feat: implement cart

--- app/Routes/cart.py
+++ app/Routes/cart.py
@@ -1,6 +1,6 @@
 from flask import request
 from app import app
-from app.models import Cart, CartItem
+from app.models import Cart, CartItem, Product, QuantityStatus, Shop, User
 from flask_login import login_required, current_user
 from app.Components.response import Response
 
@@ -15,15 +15,15 @@ def cart():
     
     if request.method == 'POST':
         data = request.get_json()
-        cart = Cart.query.filter_by(user_id=current_user.id).first()
+        cart = Cart.query.filter_by(user=current_user.id).first()
         
         if not cart:
-            cart = Cart(user_id=current_user.id)
+            cart = Cart(user=current_user.id)
             cart.create()
         
         cartItem = CartItem(
-            cart_id = cart.id,
-            item_id = data['item_id'],
+            cart = cart.id,
+            product = data['id'],
             quantity = data['quantity']
         )
 
@@ -34,18 +34,28 @@ def cart():
         )
     
     if request.method == 'GET':
-        cart = Cart.query.filter_by(user_id=current_user.id).first()
+        cart = Cart.query.filter_by(user=current_user.id).first()
         
         if not cart:
-            cart = Cart(user_id=current_user.id)
+            cart = Cart(user=current_user.id)
             cart.create()
 
-        cart_items = CartItem.query.filter_by(cart_id=cart.id).all()
+        cart_items = CartItem.query.filter_by(cart=cart.id).all()
 
         if cart_items:
-            items = {}
-            for i, item in enumerate(cart_items):
-                items[i] = item.to_dict()
+            items = []
+            for item in cart_items:
+                product = Product.query.filter_by(id=item.id).first()
+                shop = Shop.query.get(product.shop)
+                user = User.query.get(shop.user)
+
+                prod = product.to_dict(exclude=['image', 'description', 'shop', 'dateCreated', 'dateUpdated'])
+                prod['category'] = product.cat.name
+                prod['gender'] = product.gen.name
+                prod['quantity'] = item.quantity
+                prod['seller'] = user.firstName +" "+ user.lastName
+                prod['sellerAddress'] = user.address
+                items.append(prod)
 
             return Response(
                 status=200,
@@ -56,3 +66,14 @@ def cart():
             status=200,
         )
         
+@login_required
+@app.route('/api/v1/user/cart/<id>', methods=['DELETE'])
+def delete(id):
+    if request.method == 'DELETE':
+        cart = Cart.query.filter_by(user=current_user.id).first()
+        cart_item = CartItem.query.filter_by(cart=cart.id, id=id).first()
+        cart_item.delete()
+
+        return Response(
+            status=200
+        )
