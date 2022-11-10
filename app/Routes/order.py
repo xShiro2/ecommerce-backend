5eaf6ef80f4d5127d592a736c1b83fdb246e1d28
xShiro2 <daryllejumalon16@gmail.com>
2022-11-10 13:24:24 +0800

    feat: add order for admin

--- app/Routes/order.py
+++ app/Routes/order.py
@@ -2,38 +2,52 @@ from app import app, db
 from flask_login import login_required, current_user
 from flask import request
 from app.Components.response import Response
-from app.models import Order, Product
+from app.models import Order, Shop, Product, OrderStatus
 
-@app.route('/api/v1/user/cart/checkout', methods=['POST'])
+@app.route('/api/v1/admin/orders', methods=['GET'])
 @login_required
-def order():
-    if current_user.userType == 'Seller':
+def orders():
+    if current_user.userType == 'Buyer':
         return Response(
             status=403,
             message="error",
         )
+    
+    if request.method == 'GET':
+        shop = Shop.query.filter_by(user=current_user.id).first()
+        orders = Order.query.order_by(Order.dateCreated.desc()).join(Product.query.filter_by(shop=shop.id)).all()
+        
+        data = []
+        for order in orders:
+            detail = {}
+            status = OrderStatus.query.get(order.status)
+            product = Product.query.get(order.product)
+            detail['id'] = order.id
+            detail['productID'] = product.id
+            detail['productName'] = product.productName
+            detail['price'] = product.price
+            detail['quantity'] = order.quantity
+            detail['totalprice'] = order.quantity * product.price
+            detail['dateCreated'] = order.dateCreated
+            detail['buyer'] = {"fullname": order.fullname, "number": order.number, "address": order.address}
+            detail['status'] = status.name
+            data.append(detail)
 
-    if request.method == 'POST':
-        data = request.get_json()
-        product = Product.query.get(data['item_id'])
-        quantity = data['quantity']
-
-        if product.stocks < quantity or product.stocks == 0: 
-            return Response(
-                status=401,
-                message="error"
-            )
-
-        order = Order(
-            buyer_id = current_user.id,
-            item_id = product.id,
-            quantity = quantity,
+        return Response(
+            status=200,
+            data= data
         )
 
-        order.create()
-        
+@app.route('/api/v1/admin/order/status', methods=["POST"])
+@login_required
+def orderStatus():
+    if request.method == "POST":
+        data = request.get_json()
+        order = Order.query.get(data['id'])
+        status = OrderStatus.query.filter_by(name=data['status']).first()
+        order.status = status.id
+        order.update()
+
         return Response(
             status=200
         )
-
-
