c9e86ef4e09fd108ff9a6b694bb50bdca66017a0
xShiro2 <daryllejumalon16@gmail.com>
2022-11-17 10:42:59 +0800

    feat: create sample data for orders

--- app/Test/Test.py
+++ app/Test/Test.py
@@ -1,9 +1,11 @@
-from app.models import Sold, User, Shop, Product, Gender, Category, QuantityStatus
+from app.models import Sold, User, Shop, Product, Gender, Category, QuantityStatus, Cart, CartItem, Order, OrderStatus
 from werkzeug.security import generate_password_hash
 import os
 from PIL import Image
 import uuid
 import random
+import pandas as pd
+from datetime import datetime
 
 DESCRIPTION = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 PATH = 'app/Test/Images'
@@ -102,6 +104,49 @@ def save_img(img_path):
 
     return name
 
+def purchaseProduct(id, perday, start, end):
+    print('Creating sample for purchased products...')
+    fullname = "Full Name"
+    num = 99999999
+    address = "Location, Iloilo City"
+
+    products = Product.query.join(QuantityStatus.query.filter_by(status=True)).all()
+
+    dates = createDates(start, end)
+
+    for date in dates:
+        for i in range(perday):
+            product = random.choice(products).id
+            quantity = random.randint(1, 500)
+            quantityStatus = QuantityStatus.query.filter_by(product=product).first()
+
+            order = Order(
+                user=id,
+                product=product,
+                quantity=quantity,
+                fullname=fullname,
+                number=num,
+                address=address,
+                status = 1,
+                dateCreated = date
+            )
+
+            order.create()
+
+            quantityStatus.quantity -= quantity
+            quantityStatus.update()
+
+            sold = Sold.query.filter_by(product=product).first()
+            sold.quantity += quantity
+            sold.update()
+
+    print('Done!!!')
+
+def createDates(start, end):
+    dates = pd.date_range(start, end)
+
+    return dates
+
 def start():
     user = createUser(
         email="test@gmail.com",
@@ -115,6 +160,16 @@ def start():
 
     createProducts(user, 10)
 
+    buyer = createUser(
+        email="buyer@gmail.com",
+        password="test",
+        usertype='Buyer'
+    )
+
+    start = '2022-1-1'
+    end = datetime.now()
+
+    purchaseProduct(buyer, 1, start, end)
 
       
 
